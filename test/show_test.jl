122339bf4bd047424c613696aab892be246e6c65
Nils Kintscher <kintscher@math.uni-wuppertal.de>
2020-03-10 17:24:21 +0100

    fixed printf for nightly

--- test/show_test.jl
+++ test/show_test.jl
@@ -3,5 +3,5 @@ using Test
 
 @testset "show.jl" begin
     @test sprint(show, MIME("text/plain"), alfa.Crystal()) == "Lattice Basis: alfa.Lattice{2}([1.0 0.0; 0.0 1.0])\nDomain: 1-element Array{StaticArrays.SArray{Tuple{2},Float64,1,2},1}:\n [0.0, 0.0]\nCodomain: 1-element Array{StaticArrays.SArray{Tuple{2},Float64,1,2},1}:\n [0.0, 0.0]"
-    @test sprint(show, MIME("text/plain"), alfa.gallery()) ==  "Lattice Basis: alfa.Lattice{2}([1.0 0.0; 0.0 1.0])\nDomain: 1-element Array{StaticArrays.SArray{Tuple{2},Float64,1,2},1}:\n [0.0, 0.0]\nCodomain: 1-element Array{StaticArrays.SArray{Tuple{2},Float64,1,2},1}:\n [0.0, 0.0]\nMultiplier: 5-element Array{alfa.Multiplier,1}:\n alfa.Multiplier{2}([-1, 0], [1])\n alfa.Multiplier{2}([0, -1], [1])\n alfa.Multiplier{2}([0, 0], [-4])\n alfa.Multiplier{2}([0, 1], [1]) \n alfa.Multiplier{2}([1, 0], [1]) "
+    @test sprint(show, MIME("text/plain"), alfa.gallery()) ==  "Lattice Basis: alfa.Lattice{2}([1.0 0.0; 0.0 1.0])\nDomain: 1-element Array{StaticArrays.SArray{Tuple{2},Float64,1,2},1}:\n [0.0, 0.0]\nCodomain: 1-element Array{StaticArrays.SArray{Tuple{2},Float64,1,2},1}:\n [0.0, 0.0]\nMultiplier: 5-element Array{alfa.Multiplier,1}:\n alfa.Multiplier{2}([-1, 0], [1])\n alfa.Multiplier{2}([0, -1], [1])\n alfa.Multiplier{2}([0, 0], [-4])\n alfa.Multiplier{2}([0, 1], [1]) \n alfa.Multiplier{2}([1, 0], [1])"
 end
