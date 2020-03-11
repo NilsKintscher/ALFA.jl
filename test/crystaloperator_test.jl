9ec2a9c3074754603333405fbc93dcfb0054c113
Nils Kintscher <kintscher@math.uni-wuppertal.de>
2020-03-11 11:55:55 +0100

    test Snewn fix

--- test/crystaloperator_test.jl
+++ test/crystaloperator_test.jl
@@ -56,68 +56,199 @@ const atol = 1e-14
 
 
     # test find_multiplier
-    @test alfa.find_multiplier(O,[12, 12]) == nothing
-    @test alfa.find_multiplier(O,[0, 0]) isa alfa.Multiplier
+    @test alfa.find_multiplier(O, [12, 12]) == nothing
+    @test alfa.find_multiplier(O, [0, 0]) isa alfa.Multiplier
 
 
     # test wrtLattice
     S = alfa.gallery.Laplace2D()
-    S2 = alfa.wrtLattice(S, 2*S.C.L.A)
+    S2 = alfa.wrtLattice(S, 2 * S.C.L.A)
     mult = alfa.Multiplier([0, 0], [-4 1 1 0; 1 -4 0 1; 1 0 -4 1; 0 1 1 -4])
     mult_orig = deepcopy(mult)
     @test alfa.find_multiplier(push!(S2, mult), [0, 0]).mat == mult_orig.mat
-    @test alfa.find_multiplier(push!(S2, mult, true), [0, 0]).mat == 2*mult_orig.mat
+    @test alfa.find_multiplier(push!(S2, mult, true), [0, 0]).mat ==
+          2 * mult_orig.mat
     # @test alfa.find_multiplier(push!(O, mult), [0, 0]).mat == [-4]
     # @test alfa.find_multiplier(push!(O, mult, true), [0, 0]).mat == [-8]
 
 
     # check of ==
     @test S == alfa.wrtLattice(S, S.C.L)
-    S2 = alfa.wrtLattice(S, 2*S.C.L.A)
+    S2 = alfa.wrtLattice(S, 2 * S.C.L.A)
     @test S2 == deepcopy(S2)
     S3 = deepcopy(S2)
     push!(S3, mult, true)
     @test S2 != S3
     S3 = deepcopy(S2)
-    push!(S3, alfa.Multiplier([1000, 100], [-4 1 1 5; 2 -4 0 1; 1 0 -4 1; 8 1 1 -4]), true)
+    push!(
+        S3,
+        alfa.Multiplier([1000, 100], [-4 1 1 5; 2 -4 0 1; 1 0 -4 1; 8 1 1 -4]),
+        true,
+    )
     @test S2 != S3
     @test S != S2
 
     ##test symbol
-    S2 = alfa.wrtLattice(S, 2*S.C.L.A)
+    S2 = alfa.wrtLattice(S, 2 * S.C.L.A)
     x = [0.5969463404190447, 0.9794807414680586]
-    sym = Complex{Float64}[-4.0 + 0.0im 1.3452758388167156 - 0.938501249402159im 1.966939802419922 + 0.25500474210516544im 0.0 + 0.0im; 1.3452758388167156 + 0.938501249402159im -4.0 + 0.0im 0.0 + 0.0im 1.966939802419922 + 0.25500474210516544im; 1.966939802419922 - 0.25500474210516544im 0.0 + 0.0im -4.0 + 0.0im 1.3452758388167156 - 0.938501249402159im; 0.0 + 0.0im 1.966939802419922 - 0.25500474210516544im 1.3452758388167156 + 0.938501249402159im -4.0 + 0.0im]
-    @test isapprox(sym, alfa.symbol(S2,x), atol=atol)
+    sym = Complex{Float64}[
+        -4.0 + 0.0im 1.3452758388167156 - 0.938501249402159im 1.966939802419922 +
+                                                              0.25500474210516544im 0.0 +
+                                                                                    0.0im
+        1.3452758388167156 + 0.938501249402159im -4.0 + 0.0im 0.0 + 0.0im 1.966939802419922 +
+                                                                          0.25500474210516544im
+        1.966939802419922 - 0.25500474210516544im 0.0 + 0.0im -4.0 + 0.0im 1.3452758388167156 -
+                                                                           0.938501249402159im
+        0.0 + 0.0im 1.966939802419922 - 0.25500474210516544im 1.3452758388167156 +
+                                                              0.938501249402159im -4.0 +
+                                                                                  0.0im
+    ]
+    @test isapprox(sym, alfa.symbol(S2, x), atol = atol)
 
-    ev = [-0.3763088603685248, -3.656889100285505, -4.343110899714498, -7.623691139631474]
-    @test isapprox(ev, alfa.eigvals(S2,x), atol=atol)
-    @test isapprox(sort(ev, by=real), sort(alfa.eigvals(S2,x,by=nothing), by=real),  atol=atol)
+    ev = [
+        -0.3763088603685248,
+        -3.656889100285505,
+        -4.343110899714498,
+        -7.623691139631474,
+    ]
+    @test isapprox(ev, alfa.eigvals(S2, x), atol = atol)
+    @test isapprox(
+        sort(ev, by = real),
+        sort(alfa.eigvals(S2, x, by = nothing), by = real),
+        atol = atol,
+    )
 
-    evN2 = [1.3322676295501878e-15, -2.0, -2.0, -2.0, -2.0, -3.9999999999999973, -3.999999999999998, -3.9999999999999996, -4.0, -4.0, -4.0, -6.0, -6.0, -6.0, -6.0, -7.999999999999998]
-    @test isapprox(evN2, alfa.eigvals(S2,N=2), atol=atol)
-    evN2unique = Complex[0.0 + 0.0im, -2.0 + 0.0im, -4.0 + 0.0im, -6.0 + 0.0im, -8.0 + 0.0im]
-    @test isapprox(evN2unique, alfa.eigvals(S2, N=2, unique=true), atol=atol)
+    evN2 = [
+        1.3322676295501878e-15,
+        -2.0,
+        -2.0,
+        -2.0,
+        -2.0,
+        -3.9999999999999973,
+        -3.999999999999998,
+        -3.9999999999999996,
+        -4.0,
+        -4.0,
+        -4.0,
+        -6.0,
+        -6.0,
+        -6.0,
+        -6.0,
+        -7.999999999999998,
+    ]
+    @test isapprox(evN2, alfa.eigvals(S2, N = 2), atol = atol)
+    evN2unique =
+        Complex[0.0+0.0im, -2.0+0.0im, -4.0+0.0im, -6.0+0.0im, -8.0+0.0im]
+    @test isapprox(
+        evN2unique,
+        alfa.eigvals(S2, N = 2, unique = true),
+        atol = atol,
+    )
 
-    eigenX = LinearAlgebra.Eigen{Complex{Float64},Float64,Array{Complex{Float64},2},Array{Float64,1}}([-0.3763088603685248, -3.656889100285501, -4.343110899714497, -7.623691139631462], Complex{Float64}[-0.44345000006130575 + 0.23098072959800614im -0.4434500000613082 + 0.23098072959800736im 0.44345000006130586 - 0.2309807295980063im 0.4434500000613058 - 0.23098072959800625im; -0.49585025491824697 - 0.06428471589351338im 0.49585025491824525 + 0.06428471589351344im 0.4958502549182475 + 0.0642847158935136im -0.4958502549182465 - 0.06428471589351344im; -0.4100725299896222 + 0.2860778218385873im -0.4100725299896212 + 0.2860778218385863im -0.4100725299896234 + 0.28607782183858815im -0.4100725299896216 + 0.2860778218385871im; -0.5000000000000002 - 0.0im 0.5000000000000007 + 0.0im -0.49999999999999806 - 0.0im 0.5000000000000012 - 0.0im])
+    eigenX = LinearAlgebra.Eigen{
+        Complex{Float64},
+        Float64,
+        Array{Complex{Float64},2},
+        Array{Float64,1},
+    }(
+        [
+            -0.3763088603685248,
+            -3.656889100285501,
+            -4.343110899714497,
+            -7.623691139631462,
+        ],
+        Complex{Float64}[
+            -0.44345000006130575 + 0.23098072959800614im -0.4434500000613082 +
+                                                         0.23098072959800736im 0.44345000006130586 -
+                                                                               0.2309807295980063im 0.4434500000613058 -
+                                                                                                    0.23098072959800625im
+            -0.49585025491824697 - 0.06428471589351338im 0.49585025491824525 +
+                                                         0.06428471589351344im 0.4958502549182475 +
+                                                                               0.0642847158935136im -0.4958502549182465 -
+                                                                                                    0.06428471589351344im
+            -0.4100725299896222 + 0.2860778218385873im -0.4100725299896212 +
+                                                       0.2860778218385863im -0.4100725299896234 +
+                                                                            0.28607782183858815im -0.4100725299896216 +
+                                                                                                  0.2860778218385871im
+            -0.5000000000000002 - 0.0im 0.5000000000000007 + 0.0im -0.49999999999999806 -
+                                                                   0.0im 0.5000000000000012 -
+                                                                         0.0im
+        ],
+    )
 
-    for i in 1:4
-        @test isapprox(+eigenX.values[i], alfa.eigen(S2, x).values[i], atol=atol)
-        @test isapprox(eigenX.vectors[:,i], alfa.eigen(S2, x).vectors[:,i], atol=atol) || isapprox(-eigenX.vectors[:,i], alfa.eigen(S2, x).vectors[:,i], atol=atol)
+    for i = 1:4
+        @test isapprox(
+            +eigenX.values[i],
+            alfa.eigen(S2, x).values[i],
+            atol = atol,
+        )
+        @test isapprox(
+            eigenX.vectors[:, i],
+            alfa.eigen(S2, x).vectors[:, i],
+            atol = atol,
+        ) || isapprox(
+            -eigenX.vectors[:, i],
+            alfa.eigen(S2, x).vectors[:, i],
+            atol = atol,
+        )
     end
 
 
     k = Array{Float64,1}[[0.0, 0.0], [0.5, 0.0], [0.0, 0.5], [0.5, 0.5]]
-    dAk = StaticArrays.SArray{Tuple{2},Float64,1,2}[[0.0, 0.0], [0.25, 0.0], [0.0, 0.25], [0.25, 0.25]]
-    Λ = Array{Float64,1}[[1.3322676295501878e-15, -3.9999999999999973, -4.0, -7.999999999999998], [-2.0, -2.0, -6.0, -6.0], [-2.0, -2.0, -6.0, -6.0], [-3.999999999999998, -3.9999999999999996, -4.0, -4.0]]
+    dAk = StaticArrays.SArray{Tuple{2},Float64,1,2}[
+        [0.0, 0.0],
+        [0.25, 0.0],
+        [0.0, 0.25],
+        [0.25, 0.25],
+    ]
+    Λ = Array{Float64,1}[
+        [1.3322676295501878e-15, -3.9999999999999973, -4.0, -7.999999999999998],
+        [-2.0, -2.0, -6.0, -6.0],
+        [-2.0, -2.0, -6.0, -6.0],
+        [-3.999999999999998, -3.9999999999999996, -4.0, -4.0],
+    ]
     df = DataFrame(k = k, dAk = dAk, Λ = Λ)
-    @test isapprox(alfa.compute_spectrum(S2, N=2).k, k, atol=atol)
-    @test isapprox(alfa.compute_spectrum(S2, N=2).dAk, dAk, atol=atol)
-    @test isapprox(alfa.compute_spectrum(S2, N=2).Λ,  Λ, atol=atol)
+    @test isapprox(alfa.compute_spectrum(S2, N = 2).k, k, atol = atol)
+    @test isapprox(alfa.compute_spectrum(S2, N = 2).dAk, dAk, atol = atol)
+    @test isapprox(alfa.compute_spectrum(S2, N = 2).Λ, Λ, atol = atol)
 
 
 
-    S3n = alfa.CrystalOperator(alfa.Crystal(alfa.Lattice([2.0 0.0; 0.0 2.0]), SArray{Tuple{2},Float64,1,2}[[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]], SArray{Tuple{2},Float64,1,2}[[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]), SortedSet(alfa.Multiplier[alfa.Multiplier([-1, 0], [0 0 1 0; 0 0 0 1; 0 0 0 0; 0 0 0 0]), alfa.Multiplier([0, -1], [0 1 0 0; 0 0 0 0; 0 0 0 1; 0 0 0 0]), alfa.Multiplier([0, 0], [-4 1 1 0; 1 -4 0 1; 1 0 -4 1; 0 1 1 -4]), alfa.Multiplier([0, 1], [0 0 0 0; 1 0 0 0; 0 0 0 0; 0 0 1 0]), alfa.Multiplier([1, 0], [0 0 0 0; 0 0 0 0; 1 0 0 0; 0 1 0 0]), alfa.Multiplier([1000, 100], [-4 1 1 5; 1 -4 0 1; 2 0 -4 1; 8 1 1 -4])],
-       Base.Order.ForwardOrdering()), false)
+    S3n = alfa.CrystalOperator(
+        alfa.Crystal(
+            alfa.Lattice([2.0 0.0; 0.0 2.0]),
+            SArray{Tuple{2},Float64,1,2}[
+                [0.0, 0.0],
+                [0.0, 1.0],
+                [1.0, 0.0],
+                [1.0, 1.0],
+            ],
+            SArray{Tuple{2},Float64,1,2}[
+                [0.0, 0.0],
+                [0.0, 1.0],
+                [1.0, 0.0],
+                [1.0, 1.0],
+            ],
+        ),
+        SortedSet(
+            alfa.Multiplier[
+                alfa.Multiplier([-1, 0], [0 0 1 0; 0 0 0 1; 0 0 0 0; 0 0 0 0]),
+                alfa.Multiplier([0, -1], [0 1 0 0; 0 0 0 0; 0 0 0 1; 0 0 0 0]),
+                alfa.Multiplier(
+                    [0, 0],
+                    [-4 1 1 0; 1 -4 0 1; 1 0 -4 1; 0 1 1 -4],
+                ),
+                alfa.Multiplier([0, 1], [0 0 0 0; 1 0 0 0; 0 0 0 0; 0 0 1 0]),
+                alfa.Multiplier([1, 0], [0 0 0 0; 0 0 0 0; 1 0 0 0; 0 1 0 0]),
+                alfa.Multiplier(
+                    [1000, 100],
+                    [-4 1 1 5; 1 -4 0 1; 2 0 -4 1; 8 1 1 -4],
+                ),
+            ],
+            Base.Order.ForwardOrdering(),
+        ),
+        false,
+    )
 
     @test S3n == alfa.normalize(S3)
 
@@ -126,17 +257,82 @@ const atol = 1e-14
     R = alfa.gallery.fw_restriction2D()
     X = alfa.Lattice([0.0 -2.0; 2.0 2.0])
 
-    Rnew = alfa.CrystalOperator(alfa.Crystal(alfa.Lattice([0.0 -2.0; 2.0 2.0]), SArray{Tuple{2},Float64,1,2}[[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]], SArray{Tuple{2},Float64,1,2}[[0.0, 0.0]]), SortedSet(alfa.Multiplier[alfa.Multiplier([-2, 1], Rational{Int64}[0//1 0//1 0//1 1//4]), alfa.Multiplier([-1, 0], Rational{Int64}[0//1 1//2 0//1 1//4]), alfa.Multiplier([-1, 1], Rational{Int64}[0//1 0//1 1//2 1//4]), alfa.Multiplier([0, 0], Rational{Int64}[1//1 1//2 1//2 1//4])],
+    Rnew = alfa.CrystalOperator(
+        alfa.Crystal(
+            alfa.Lattice([0.0 -2.0; 2.0 2.0]),
+            SArray{Tuple{2},Float64,1,2}[
+                [0.0, 0.0],
+                [0.0, 1.0],
+                [1.0, 0.0],
+                [1.0, 1.0],
+            ],
+            SArray{Tuple{2},Float64,1,2}[[0.0, 0.0]],
+        ),
+        SortedSet(
+            alfa.Multiplier[
+                alfa.Multiplier(
+                    [-2, 1],
+                    Rational{Int64}[0 // 1 0 // 1 0 // 1 1 // 4],
+                ),
+                alfa.Multiplier(
+                    [-1, 0],
+                    Rational{Int64}[0 // 1 1 // 2 0 // 1 1 // 4],
+                ),
+                alfa.Multiplier(
+                    [-1, 1],
+                    Rational{Int64}[0 // 1 0 // 1 1 // 2 1 // 4],
+                ),
+                alfa.Multiplier(
+                    [0, 0],
+                    Rational{Int64}[1 // 1 1 // 2 1 // 2 1 // 4],
+                ),
+            ],
+            Base.Order.ForwardOrdering(),
+        ),
+        false,
+    )
+    @test Rnew == alfa.wrtLattice(R, X)
+
+    Rnewn = alfa.CrystalOperator(
+        alfa.Crystal(
+            alfa.Lattice([0.0 -2.0; 2.0 2.0]),
+            StaticArrays.SArray{Tuple{2},Float64,1,2}[
+                [-1.0, 1.0],
+                [-1.0, 2.0],
+                [0.0, 0.0],
+                [0.0, 1.0],
+            ],
+            StaticArrays.SArray{Tuple{2},Float64,1,2}[[0.0, 0.0]],
+        ),
+        SortedSet(
+            alfa.Multiplier[
+                alfa.Multiplier(
+                    [-1, 0],
+                    Rational{Int64}[1 // 4 1 // 2 0 // 1 1 // 2],
+                ),
+                alfa.Multiplier(
+                    [0, -1],
+                    Rational{Int64}[1 // 4 1 // 2 0 // 1 0 // 1],
+                ),
+                alfa.Multiplier(
+                    [0, 0],
+                    Rational{Int64}[1 // 4 0 // 1 1 // 1 1 // 2],
+                ),
+                alfa.Multiplier(
+                    [1, -1],
+                    Rational{Int64}[1 // 4 0 // 1 0 // 1 0 // 1],
+                ),
+            ],
+            Base.Order.ForwardOrdering(),
+        ),
+        false,
+    )
+
+    Snewn = alfa.CrystalOperator(alfa.Crystal(alfa.Lattice([0.0 -2.0; 2.0 2.0]), SArray{Tuple{2},Float64,1,2}[[-1.0, 1.0], [-1.0, 2.0], [0.0, 0.0], [0.0, 1.0]], SArray{Tuple{2},Float64,1,2}[[-1.0, 1.0], [-1.0, 2.0], [0.0, 0.0], [0.0, 1.0]]), SortedSet(alfa.Multiplier[alfa.Multiplier([-1, 0], [0 1 0 0; 0 0 0 0; 0 1 0 1; 0 0 0 0]), alfa.Multiplier([-1, 1], [0 0 0 1; 0 0 0 0; 0 0 0 0; 0 0 0 0]), alfa.Multiplier([0, -1], [0 0 0 0; 0 0 0 0; 0 1 0 0; 0 0 0 0]), alfa.Multiplier([0, 0], [-4 1 0 1; 1 -4 0 0; 0 0 -4 1; 1 0 1 -4]), alfa.Multiplier([0, 1], [0 0 0 0; 0 0 1 0; 0 0 0 0; 0 0 0 0]), alfa.Multiplier([1, -1], [0 0 0 0; 0 0 0 0; 0 0 0 0; 1 0 0 0]), alfa.Multiplier([1, 0], [0 0 0 0; 1 0 1 0; 0 0 0 0; 0 0 1 0])],
        Base.Order.ForwardOrdering()), false)
-    @test Rnew == alfa.wrtLattice(R,X)
 
-    Rnewn = alfa.CrystalOperator(alfa.Crystal(alfa.Lattice([0.0 -2.0; 2.0 2.0]), StaticArrays.SArray{Tuple{2},Float64,1,2}[[-1.0, 1.0], [-1.0, 2.0], [0.0, 0.0], [0.0, 1.0]], StaticArrays.SArray{Tuple{2},Float64,1,2}[[0.0, 0.0]]), SortedSet(alfa.Multiplier[alfa.Multiplier([-1, 0], Rational{Int64}[1//4 1//2 0//1 1//2]), alfa.Multiplier([0, -1], Rational{Int64}[1//4 1//2 0//1 0//1]), alfa.Multiplier([0, 0], Rational{Int64}[1//4 0//1 1//1 1//2]), alfa.Multiplier([1, -1], Rational{Int64}[1//4 0//1 0//1 0//1])], Base.Order.ForwardOrdering()), false)
-
-    Snewn = alfa.CrystalOperator(alfa.Crystal(alfa.Lattice([0.0 -2.0; 2.0 2.0]), SArray{Tuple{2},Float64,1,2}[[-1.0, 1.0], [-1.0, 2.0], [0.0, 0.0], [0.0, 1.0]], SArray{Tuple{2},Float64,1,2}[[0.0, 0.0]]), SortedSet(alfa.Multiplier[alfa.Multiplier([-1, 0], Rational{Int64}[1//4 1//2 0//1 1//2]), alfa.Multiplier([0, -1], Rational{Int64}[1//4 1//2 0//1 0//1]), alfa.Multiplier([0, 0], Rational{Int64}[1//4 0//1 1//1 1//2]), alfa.Multiplier([1, -1], Rational{Int64}[1//4 0//1 0//1 0//1])],
-           Base.Order.ForwardOrdering()), false)
-
-           @test Snewn == alfa.wrtSameLatticeAndNormalize(S,R)[1]
-    @test Rnewn == alfa.wrtSameLatticeAndNormalize(S,R)[2]
+    @test Snewn == alfa.wrtSameLatticeAndNormalize(S, R)[1]
+    @test Rnewn == alfa.wrtSameLatticeAndNormalize(S, R)[2]
     #
 
 
