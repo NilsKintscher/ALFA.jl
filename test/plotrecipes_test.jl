f04182e862ee09441965f9205ced29c1ce2a129f
NilsKintscher <kintscher@math.uni-wuppertal.de>
2020-03-19 14:00:03 +0100

    fixed plotrecipes test.

--- test/plotrecipes_test.jl
+++ test/plotrecipes_test.jl
@@ -4,13 +4,13 @@ using Plots
 
 # test if plot functions throw an error.
 @testset "plotrecipes.jl" begin
-    R = alfa.gallery.fw_restriction(2,Float64)
+    R = alfa.gallery.fw_restriction(N=2,T=Float64)
 
     @test plot(R.C.L) isa Plots.Plot{Plots.GRBackend}
     @test plot(R.C) isa Plots.Plot{Plots.GRBackend}
     @test plot(R) isa Plots.Plot{Plots.GRBackend}
 
-    S = alfa.gallery.Laplace(2,Float64)
+    S = alfa.gallery.Laplace(N=2,T=Float64)
     S2 = alfa.wrtLattice(S, S.C.L.A*2)
     @test surfacespectrum(S2) isa Plots.Plot{Plots.GRBackend}
     @test surfacespectrum(S2,4) isa Plots.Plot{Plots.GRBackend}
