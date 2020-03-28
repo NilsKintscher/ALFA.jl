30b8388cd671da4c22bf37fd036402918ef52f04
NilsKintscher <kintscher@math.uni-wuppertal.de>
2020-03-28 15:30:01 +0100

    added tests

--- test/operatorcomposition_test.jl
+++ test/operatorcomposition_test.jl
@@ -1,10 +1,20 @@
 using alfa
 using Test
+using LinearAlgebra
 
 @testset "operatorcomposition.jl" begin
         for T in [Float64, Rational{BigInt}]
+                 L = alfa.gallery.Laplace(T=T)
+                 LL =L*L
+                 f = :($L*$L)
+                 oc = alfa.OperatorComposition(f)
+                 k = randn(2)
+                 @test alfa.symbol(oc,k) ≈ alfa.symbol(LL,k)
+                 @test alfa.eigvals(oc,k) ≈ alfa.eigvals(LL,k)
 
+                 f = :(I-$L*inv($L) +adjoint($L) - transpose($L) )
+                 oc = alfa.OperatorComposition(f)
+                 @test isapprox(norm(alfa.eigvals(oc,k)),0, atol=1e-20)
 
-
-end
+        end
 end
