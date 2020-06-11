5edead662ca87a73df77938f6193eaadbf03e3f0
NilsKintscher <kintscher@math.uni-wuppertal.de>
2020-06-11 16:31:24 +0200

    added 1d plot tests

--- test/plotrecipes_test.jl
+++ test/plotrecipes_test.jl
@@ -4,15 +4,29 @@ using Plots
 
 # test if plot functions throw an error.
 @testset "plotrecipes.jl" begin
-    R = ALFA.gallery.fw_restriction(N=2,T=Float64)
+        R = ALFA.gallery.fw_restriction(N = 1, T = Float64)
 
-    @test plot(R.C.L) isa Plots.Plot{Plots.GRBackend}
-    @test plot(R.C) isa Plots.Plot{Plots.GRBackend}
-    @test plot(R) isa Plots.Plot{Plots.GRBackend}
+        @test plot(R.C.L) isa Plots.Plot{Plots.GRBackend}
+        @test plot(R.C) isa Plots.Plot{Plots.GRBackend}
+        @test plot(R) isa Plots.Plot{Plots.GRBackend}
 
-    S = ALFA.gallery.Laplace(N=2,T=Float64)
-    S2 = ALFA.wrtLattice(S, S.C.L.A*2)
-    @test surfacespectrum(S2) isa Plots.Plot{Plots.GRBackend}
-    @test surfacespectrum(S2,4) isa Plots.Plot{Plots.GRBackend}
+        S = ALFA.gallery.Laplace(N = 1, T = Float64)
+        S2 = ALFA.wrtLattice(S, S.C.L.A * 2)
+        @test plotspectrum(S2) isa Plots.Plot{Plots.GRBackend}
+        @test plotspectrum(S2, 4) isa Plots.Plot{Plots.GRBackend}
 
+
+
+
+
+        R = ALFA.gallery.fw_restriction(N = 2, T = Float64)
+
+        @test plot(R.C.L) isa Plots.Plot{Plots.GRBackend}
+        @test plot(R.C) isa Plots.Plot{Plots.GRBackend}
+        @test plot(R) isa Plots.Plot{Plots.GRBackend}
+
+        S = ALFA.gallery.Laplace(N = 2, T = Float64)
+        S2 = ALFA.wrtLattice(S, S.C.L.A * 2)
+        @test plotspectrum(S2) isa Plots.Plot{Plots.GRBackend}
+        @test plotspectrum(S2, 4) isa Plots.Plot{Plots.GRBackend}
 end
